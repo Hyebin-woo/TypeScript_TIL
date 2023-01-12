5ec8c84218619a5c7bed2e4bd9b347ac8ae9918d
Hyebin-Woo <dev.hyebin@gmail.com>
2023-01-12 22:01:03 +0900

    이름변경

---
+++ 05_alias&readonly/hw.js
@@ -0,0 +1,17 @@
+{
+    var testA = {
+        size: 123,
+        position: [1, 2, 3],
+    };
+}
+{
+    var mem = {
+        name: "kim",
+        phone: 123,
+    };
+    var 회원가입정보 = {
+        name: "kim",
+        adult: false,
+        phone: 1234,
+    };
+}
